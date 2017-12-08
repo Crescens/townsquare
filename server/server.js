adaa6c1b276e755d4f858ec6c1b5af99c23f28b2
justintwynn <justintwynn@gmail.com>
2017-12-07 19:01:49 -0500

    Remove Raven dependency until/unless we want to use it again

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const api = require('./api');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const http = require('http');
-const Raven = require('raven');
+//const Raven = require('raven');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpack = require('webpack');
@@ -21,7 +21,7 @@ const webpackConfig = require('../webpack.config.js');
 const pug = require('pug');
 
 const UserService = require('./repositories/UserService.js');
-const version = require('../version.js');
+//const version = require('../version.js');
 
 const defaultWindows = {
     plot: false,
