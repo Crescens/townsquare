067fdc302c3e07796af53649b0ca3e90a17c86c0
Marcin Swiderski <mswiderski2012@gmail.com>
2018-06-20 22:54:43 +0100

    Winner check and unbooting cards during Sundown.

--- server/game/gamesteps/sundownphase.js
+++ server/game/gamesteps/sundownphase.js
@@ -7,52 +7,45 @@ class SundownPhase extends Phase {
     constructor(game) {
         super(game, 'sundown');
         this.initialise([
-            new SimpleStep(game, () => this.resetPlayerStatus()),
-            new SimpleStep(game, () => this.payUpkeep()),
+            new SimpleStep(game, () => this.unbootCards()),
+            new SimpleStep(game, () => this.checkWinCondition()),
             new ReadyPrompt(game, 'sundown')
         ]);
     }
 
+    checkWinCondition() {
+        const potentialWinner = [];
+        _.each(this.game.getPlayers(), player => {
+            let opponents = _.reject(this.game.getPlayers(), activePlayer => activePlayer === player);
+            if (_.every(opponents, opponent => opponent.getTotalInfluence() < player.getTotalControl())){
+                console.log("player control points" + player.getTotalControl());
+                potentialWinner.push(player);
+            }
+
+
+        }
+        )
+        if (potentialWinner.length === 1) {
+            this.game.recordWinner(potentialWinner[0], 'Control points greater than influence');
+        }
+        
+    }
+                 
+
+    unbootCards() { 
+        _.each(this.game.getPlayers(), player => {
+              player.cardsInPlay.each(card => {
+              player.unbootCard(card);
+            });
+        });
+    }
+    
     resetPlayerStatus() {
         _.each(this.game.getPlayers(), player => {
             player.resetForRound();
         });
     }
 
-    payUpkeep() {
-
-    }
-
-    /*
-    prepareDecks() {
-        this.game.raiseEvent('onDecksPrepared');
-        _.each(this.game.getPlayers(), player => {
-            if(player.agenda) {
-                player.agenda.applyPersistentEffects();
-            }
-        });
-        this.game.allCards.each(card => {
-            card.applyAnyLocationPersistentEffects();
-        });
-    }
-
-    drawStartingPosse() {
-        _.each(this.game.getPlayers(), player => {
-            player.drawCardsToHand('hand', player.startingPosse);
-        });
-    }
-
-    startGame() {
-        _.each(this.game.getPlayers(), player => {
-            player.startGame();
-        });
-    }
-
-    setupDone() {
-        _.each(this.game.getPlayers(), p => {
-            p.setupDone();
-        });
-    }*/
 }
 
 module.exports = SundownPhase;
