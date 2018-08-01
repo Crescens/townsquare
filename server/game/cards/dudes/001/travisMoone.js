cfb37b3da462df674f08cf6b3211f3eb012f89dd
Marcin Swiderski <mswiderski2012@gmail.com>
2018-08-01 15:29:23 +0100

    The first working implementation of Travis Moone

--- server/game/cards/dudes/001/travisMoone.js
+++ server/game/cards/dudes/001/travisMoone.js
@@ -3,10 +3,14 @@ const DrawCard = require('../../../drawcard.js');
 class TravisMoone extends DrawCard{
     setupCardAbilities(ability) {
         this.action({
-            title: 'Draw new starting hand',
+            title: 'Discard cards and draw new starting hand',
             limit: ability.limit.perPhase(1),
             handler: context => {
+                context.player.bootCard(this);
                 context.player.hand.each(card => context.player.moveCard(card, 'draw deck')); 
+                context.player.shuffleDrawDeck();
+                context.player.drawCardsToHand('hand', 5);
+                context.player.game.addMessage(`${context.player.name} used ${this.title} ability to draw new starting hand`);
             }
         });
     }
